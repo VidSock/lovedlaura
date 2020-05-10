0c7a22f224793d20df961409403ab629312f9d91
twilightscapes <ihatespam@gmail.com>
2020-05-10 08:00:46 -0500

    update 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,348 @@ module.exports = {
       author: config.userName,
       copyright: config.copyright,
     },
+
+
+
+   
+
+
+    gatsbyStorefrontConfig: {
+      storeName: 'Loved by Laura',
+      storeDescription: 'Various knick-knacks and items handmade with love, by Laura',
+      email: 'info@gatsbystorefront.com',
+      company: 'Loved by Laura',
+      location: 'Gulfport, MS',
+      address: '',
+      phone: '+1 (800) 123-1234',
+      workingDays: '',
+      workingHours: '',
+      socialNetworks: [
+        'https://facebook.com',
+        'https://instagram.com',
+        'https://pinterest.com',
+        'https://twitter.com',
+        'https://youtube.com',
+      ],
+      payments: ['visa', 'mastercard', 'amex', 'discover', 'shopify', 'paypal'],
+      // For available social share buttons see: https://github.com/nygardk/react-share
+      shareButtons: [
+        'Facebook',
+        'Pinterest',
+        'Twitter',
+        'Tumblr',
+        'Whatsapp',
+        'Line',
+        'Viber',
+      ],
+      googleAnalyticsId: 'UA-141525658-5',
+      //
+      // carousel, collection, product
+      //
+      mainPage: [
+        {
+          type: 'carousel',
+          children: [
+            {
+              name: 'Aprons',
+              type: 'collection',
+              handle: 'aprons',
+              textColor: 'black',
+              textBgColor: 'white',
+            },
+            {
+              name: 'Night Photography',
+              type: 'collection',
+              handle: 'twilightscapes-prints',
+              textColor: 'white',
+              textBgColor: 'primary',
+            },
+            {
+              name: 'Aprons',
+              type: 'collection',
+              handle: 'aprons',
+              textColor: 'black',
+              textBgColor: 'white',
+            },
+          ],
+        },
+        {
+          name: 'Apparel',
+          type: 'collection',
+          handle: 'apparel',
+          textColor: 'white',
+          textBgColor: 'primary',
+        },
+        {
+          name: 'Garden',
+          type: 'collection',
+          handle: 'garden',
+          textColor: 'white',
+          textBgColor: 'primary',
+        },
+        {
+          name: 'Test',
+          type: 'collection',
+          handle: 'test-collection',
+          textColor: 'black',
+          textBgColor: 'white',
+        },
+        {
+          name: 'One product',
+          type: 'product',
+          handle: 'red-sports-tee',
+          textColor: 'black',
+          textBgColor: 'white',
+        },
+        {
+          name: 'Anchor Bracelet Mens',
+          type: 'product',
+          handle: 'leather-anchor',
+          textColor: 'black',
+          textBgColor: 'white',
+        },
+        {
+          name: 'Yellow Sofa',
+          type: 'product',
+          handle: 'yellow-sofa',
+          textColor: 'black',
+          textBgColor: 'white',
+        },
+        {
+          name: '7 Shakra Bracelet',
+          type: 'product',
+          handle: 'chain-bracelet',
+          textColor: 'black',
+          textBgColor: 'white',
+        },
+        {
+          name: 'White Cotton Shirt',
+          type: 'product',
+          handle: 'white-cotton-shirt',
+          textColor: 'white',
+          textBgColor: 'primary',
+        },
+      ],
+      // Menu types: "header", "collection", "product", "link"
+      menu: flattenMenu({
+        name: 'Menu',
+        type: 'top',
+        children: [
+          {
+            name: "Women's",
+            type: 'header',
+            handle: '',
+            link: '',
+            children: [
+              {
+                name: 'Apparel',
+                type: 'collection',
+                handle: 'apparel',
+              },
+              {
+                name: 'Jewelery',
+                type: 'collection',
+                handle: 'jewelery',
+              },
+              {
+                name: 'One product',
+                type: 'product',
+                handle: 'red-sports-tee',
+              },
+            ],
+          },
+          {
+            name: "Men's",
+            type: 'header',
+            children: [
+              {
+                name: 'Test',
+                type: 'collection',
+                handle: 'test-collection',
+              },
+              {
+                name: 'Garden',
+                type: 'collection',
+                handle: 'garden',
+              },
+              {
+                name: 'Apparel',
+                type: 'collection',
+                handle: 'apparel',
+              },
+              {
+                name: 'External links',
+                type: 'header',
+                children: [
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 2',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                  {
+                    name: 'External link 3',
+                    type: 'external',
+                    link: 'https://amazon.com',
+                  },
+                ],
+              },
+            ],
+          },
+          { name: 'Blog', type: 'blog', handle: '' },
+        ],
+      }),
+      footerLinks: [
+        {
+          name: 'About us',
+          link: '/about',
+        },
+        {
+          name: 'Terms of Service',
+          link: '/shop/policy/termsOfService',
+        },
+        {
+          name: 'Privacy policy',
+          link: '/shop/policy/privacyPolicy',
+        },
+        {
+          name: 'Refunds',
+          link: '/shop/policy/refundPolicy',
+        },
+        {
+          name: 'Site by Todd',
+          link: 'https://toddlambert.com',
+        },
+      ],
+      locales: 'en-US',
+      currency: 'USD',
+      productsPerCollectionPage: '9',
+      articlesPerBlogPage: '6',
+    },
   },
+
+
+
+
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
